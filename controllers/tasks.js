15280e8bdaf783f7a51b63e17417e0e28b3e21ed
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-16 21:39:38 +0200

    send json

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("Create new taks");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("Get specific task");
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
