aa86a0ae0c03018e2f7b126e082f1b2b9acab3e3
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-18 15:26:09 +0200

    refactor connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 //middleware for getting the routes
 
@@ -15,4 +16,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
