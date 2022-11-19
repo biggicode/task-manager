a1085f9104ad801740a292f5b900833a6c9825e3
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-19 17:04:15 +0200

    basic validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,21 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("All tasks from the file");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
